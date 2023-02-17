59a2ed35802b52de4d061d0ab81121d5288533e9
wahooey <cc4655@columbia.edu>
2023-02-17 15:05:55 -0500

    implement scatter plots using plotly

--- spotify-charts/functions.py
+++ spotify-charts/functions.py
@@ -1,7 +1,8 @@
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
-import matplotlib.pyplot as plt
-import numpy as np
+import plotly.express as px
+import pandas as pd
+
 
 scope = "user-library-read"
 
@@ -15,43 +16,49 @@ def fetch_data(type='user', id=0, limit=20):
     return sp.current_user_saved_tracks(limit)
 
 
-def graph_audio_features(data, feature1, feature2=None, feature3=None, plot='histogram', dims=1):
-    x = []
-    y = []
-    z = []
-    for item in enumerate(data['items']):
+def graph_audio_features(input, feature1, feature2=None, feature3=None, plot='histogram', dims=1):
+    data = []
+    for item in enumerate(input['items']):
+        song_data = []
+        song_data.append(item[1]['track']['name'])
+        song_data.append(item[1]['track']['artists'][0]['name'])
+        song_data.append(item[1]['track']['album']['name'])
         track_id = item[1]['track']['uri']
-        x.append(sp.audio_features(track_id)[0][feature1])
+        song_data.append(sp.audio_features(track_id)[0][feature1])
         if dims > 1:
-            y.append(sp.audio_features(track_id)[0][feature2])
+            song_data.append(sp.audio_features(track_id)[0][feature2])
         if dims > 2:
-            z.append(sp.audio_features(track_id)[0][feature3])
+            song_data.append(sp.audio_features(track_id)[0][feature3])
+        data.append(song_data)
 
     if plot == 'histogram':
-        plt.hist(np.array(x))
+        print("lol")
     elif plot == 'scatterplot':
         if dims == 2:
-            plt.scatter(x, y)
-            plt.xlabel(feature1)
-            plt.ylabel(feature2)
+            df = pd.DataFrame(
+                data, columns=['name', 'artist', 'album', feature1, feature2])
+            fig = px.scatter(df, x=feature1, y=feature2,
+                             custom_data=['name', 'artist', 'album'])
         elif dims == 3:
-            _, ax = plt.subplots(subplot_kw={"projection": "3d"})
-            ax.scatter(x, y, z)
-            ax.set_xlabel(feature1)
-            ax.set_ylabel(feature2)
-            ax.set_zlabel(feature3)
-    plt.show()
-    # if dims < 1 or dims > 3:
-    #     raise Exception("Invalid dimensions. The supported number of dimensions are: 1, 2, 3")
-    # else:
-    #     raise Exception("Invalid plot type. The supported plot types are: 'histogram', 'scatterplot'")
+            df = pd.DataFrame(
+                data, columns=['name', 'artist', 'album', feature1, feature2, feature3])
+            fig = px.scatter_3d(df, x=feature1, y=feature2, z=feature3, custom_data=[
+                                'name', 'artist', 'album'])
+        fig.update_traces(
+            hovertemplate="<br>".join([
+                "name: %{customdata[0]}",
+                "artist: %{customdata[1]}",
+                "album: %{customdata[2]}",
+            ])
+        )
+    fig.show()
 
 
-results = fetch_data(50)
+results = fetch_data(limit=50)
 
-
-graph_audio_features(results, feature1='energy',
-                     feature2='loudness', plot='scatterplot', dims=2)
 
 # graph_audio_features(results, feature1='energy',
-#                      feature2='loudness', feature3='danceability', plot='scatterplot', dims=3)
+#                      feature2='loudness', plot='scatterplot', dims=2)
+
+graph_audio_features(results, feature1='energy',
+                     feature2='loudness', feature3='danceability', plot='scatterplot', dims=3)
