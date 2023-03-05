6efe8dc8f8bffb36933127aa2c255fee566c6e73
wahooey <cc4655@columbia.edu>
2023-03-05 14:55:01 -0500

    moved mock data into conftest.py

--- splotify/tests/test_integration.py
+++ splotify/tests/test_integration.py
@@ -2,6 +2,7 @@ from splotify import spotifyapi
 from splotify import data
 from splotify.plots import audiofeatures
 from splotify import helpers
+from splotify.tests.conftest import loudness_data, danceability_data
 
 
 def spotify_auth():
@@ -12,7 +13,7 @@ def spotify_auth():
     )
 
 
-def test_integration():
+def test_integration(loudness_data, danceability_data):
     sp = spotify_auth()
 
     d = data.Data(sp)
@@ -26,59 +27,7 @@ def test_integration():
 
     df = d.get_df()
 
-    df["loudness"] = [
-        -5.697,
-        -6.931,
-        -12.049,
-        -6.091,
-        -8.236,
-        -5.68,
-        -4.692,
-        -8.108,
-        -6.371,
-        -6.963,
-        -5.66,
-        -5.938,
-        -9.655,
-        -7.313,
-        -6.501,
-        -8.919,
-        -11.357,
-        -9.017,
-        -9.129,
-        -14.99,
-        -5.491,
-        -7.475,
-        -10.654,
-        -9.813,
-        -8.57,
-    ]
-    df["danceability"] = [
-        0.846,
-        0.708,
-        0.549,
-        0.719,
-        0.717,
-        0.728,
-        0.7,
-        0.5,
-        0.519,
-        0.589,
-        0.476,
-        0.56,
-        0.749,
-        0.305,
-        0.251,
-        0.312,
-        0.293,
-        0.351,
-        0.36,
-        0.432,
-        0.201,
-        0.243,
-        0.255,
-        0.213,
-        0.241,
-    ]
+    df["loudness"] = loudness_data
+    df["danceability"] = danceability_data
 
     assert afp.get_df().equals(df)
